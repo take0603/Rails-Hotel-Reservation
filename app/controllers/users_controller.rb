4cd80ad2d98881f4e8863114f1763f88f29f7a3c
Shohei Takeda <lucky.take.star@gmail.com>
2023-07-06 20:20:00 +0900

    userモデルにプロフィール画像アップ機能を実装、Carrierwaveの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:icon, :name, :introduction)
+    params.require(:user).permit(:icon, :name, :introduction, :user_image)
   end
 #ここまで
 
