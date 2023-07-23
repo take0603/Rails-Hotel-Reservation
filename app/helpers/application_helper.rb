6fab266a02ba8392e461b3806ff43ec82a627e18
Shohei Takeda <lucky.take.star@gmail.com>
2023-07-23 18:02:42 +0900

    ユーザー、施設それぞれ画像が登録されていない場合にデフォルト画像を表示するよう設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  # ユーザー画像の登録有無を確認し、ない場合はデフォルト画像を表示するメソッド
+  def user_image(user)
+    if user[:user_image].nil?
+      "default_user_image.png"
+    else
+      user.user_image.url
+    end
+  end
+
+  # 施設画像の登録有無を確認し、ない場合はデフォルト画像を表示するメソッド
+  def room_image(room)
+    if room[:room_image].nil?
+      "default_room_image.png"
+    else
+      room.room_image.url
+    end
+  end
+
 end
